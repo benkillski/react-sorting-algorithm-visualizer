e3958af1ec352ad7c15cb90a907c9b331b4840f4
Ben Kilkowski <benkilkowski@outlook.com>
2022-12-05 21:42:40 -0600

    Implemented Merge Sort and Animations

--- src/AlgorithmMenu.js
+++ src/AlgorithmMenu.js
@@ -5,12 +5,13 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
 
-export default function AlgorithmMenu({generateArray, array, selectedAlgorithm, algorithmOptions, setSelectedAlgorithm}) {
+export default function AlgorithmMenu({generateArray, array, selectedAlgorithm, algorithmOptions, setSelectedAlgorithm, sortFunction}) {
   return (
     <React.Fragment>
-      <AppBar color="primary" position="block">
+      <AppBar color="primary" position="relative">
       <Toolbar>
         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: "center" }}>
+          <Button onClick={sortFunction} variant="contained" sx={{mx: 10}}>Sort</Button>
           <Button onClick={generateArray} variant="contained" sx={{mx: 10}}>New Array</Button>
           <Typography sx={{mx: 10}}>Algorithm: {selectedAlgorithm}</Typography>
           <Typography sx={{mx: 10}}>Array Size: {array.length}</Typography>
--- src/AlgorithmVisualizer.js
+++ src/AlgorithmVisualizer.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import AlgorithmMenu from './AlgorithmMenu';
+import { getMergeSortAnimations } from "./Algorithms";
 
 const ALGORITHM_OPTIONS = [
     "Bubble",
@@ -25,21 +26,161 @@ const ALGORITHM_OPTIONS = [
     "Brick"
 ];
 
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'white';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
 export default function AlgorithmVisualizer(props) {
 
     const [mainArray, setMainArray] = useState([]);
     const [selectedAlgorithm, setSelectedAlgorithm] = useState([ALGORITHM_OPTIONS[0]]);
 
-    useEffect(() => {
-        generateArray();
-    }, []);
-
     function generateArray() {
-        const array = [];
+        let array = [];
         for(let i = 0; i < 200; i++) {
             array.push(Math.floor(Math.random() * 800) + 1);
         }
         setMainArray(array);
+    };
+
+    useEffect(() => {
+        generateArray();
+    }, []);
+
+    const handleSorting = () => {
+        switch (selectedAlgorithm) {
+          case 'Bubble':
+            bubbleSort();
+            break;
+          case 'Selection':
+            selectionSort();
+            break;
+          case 'Insertion':
+            insertionSort();
+            break;
+          case 'Merge':
+            mergeSort();
+            break;
+          case 'Quick':
+            quickSort();
+            break;
+          case 'Heap':
+            heapSort();
+            break;
+          default:
+            break;
+        }
+    }
+
+    function bubbleSort() {
+    }
+
+    function selectionSort() {
+        
+    }
+
+    function mergeSort() {
+        console.log("merger sort ran");
+        const animations = getMergeSortAnimations(mainArray);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('element-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+    }
+
+    function insertionSort() {
+    }
+
+    function heapSort() {
+        
+    }
+
+    function quickSort() {
+
+    }
+
+    function radixSort() {
+        
+    }
+
+    function countingSort() {
+
+    }
+
+    function bucketSort() {
+        
+    }
+
+    function shellSort() {
+
+    }
+
+    function combSort() {
+        
+    }
+
+    function pigeonholeSort() {
+
+    }
+
+    function cycleSort() {
+        
+    }
+
+    function timSort() {
+
+    }
+
+    function cocktailSort() {
+        
+    }
+
+    function bitonicSort() {
+
+    }
+
+    function pancakeSort() {
+        
+    }
+
+    function bogoSort() {
+
+    }
+
+    function gnomeSort() {
+        
+    }
+
+    function stoogeSort() {
+
+    }
+
+    function brickSort() {
+        
     }
 
     return (
@@ -50,7 +191,7 @@ export default function AlgorithmVisualizer(props) {
                     </div>
                 ))}
             </div>
-            <AlgorithmMenu algorithmOptions={ALGORITHM_OPTIONS} generateArray={generateArray} array={mainArray} selectedAlgorithm={selectedAlgorithm} setSelectedAlgorithm={setSelectedAlgorithm}/>
+            <AlgorithmMenu algorithmOptions={ALGORITHM_OPTIONS} generateArray={generateArray} array={mainArray} selectedAlgorithm={selectedAlgorithm} setSelectedAlgorithm={setSelectedAlgorithm} sortFunction={handleSorting}/>
         </>
     );
 }
